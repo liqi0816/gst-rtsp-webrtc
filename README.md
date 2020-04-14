5f1509458d0fc4b7207341f379bbbf98471b1ba5
LI, Qi <goodlq11@gmail.com>
2020-04-14 13:28:26 -0400

    0414c

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Description
 
-This project demonstrates how to use [gstreamer](https://gstreamer.freedesktop.org) to covert a RTSP stream into a WebRTC stream, and how to feed it into a browser.
+This project demonstrates how to use [gstreamer](https://gstreamer.freedesktop.org) to covert an RTSP stream into a WebRTC stream, and how to feed it into a browser.
 
 This work is based on [centricular/gstwebrtc-demos](https://github.com/centricular/gstwebrtc-demos).
 
@@ -40,7 +40,7 @@ pip3 install websockets
 
 ## Prepare local RTSP server (optional)
 
-There exists [a public RTSP test stream](https://www.wowza.com/html/mobile.html) hosted by wowza.com, but there is no grantee that it will be there forever. In addition, it seems to have a rate limit and behaves weird if requested too many times. 
+There exists [a public RTSP test stream](https://www.wowza.com/html/mobile.html) hosted by wowza.com, but there is no guarantee that it will be there forever. In addition, it seems to have a rate limit and behaves weird if requested too many times. 
 
 ### Install ffmpeg
 
@@ -61,13 +61,47 @@ npm install
 wget http://ia800501.us.archive.org/10/items/BigBuckBunny_310/big_buck_bunny_640_512kb.mp4
 ```
 
+### Run rtsp-streaming-server
+
+```bash
+node index.js
+```
+
 ### Run stream push script
 
 ```bash
 while true; do
-	ffmpeg -re -i big_buck_bunny_640_512kb.mp4 -c:v copy -f rtsp rtsp://127.0.0.1:5554/stream1
+    ffmpeg -re -i big_buck_bunny_640_512kb.mp4 -c:v copy -f rtsp rtsp://127.0.0.1:5554/stream1
 done
 ```
 
 # Demo
 
+Open the [public demo webpage](https://webrtc.nirbheek.in). Note your `id`. Run:
+
+```bash
+python3 streamer.py YOUR_ID
+```
+
+The log should be like:
+
+```
+Sending offer:
+[blah]=[blahblah]
+
+Received answer:
+[blah]=[blahblah]
+```
+
+Then the demo webpage should be streaming video.
+
+# Troubleshooting
+
+## No log output
+
+1. You are using the public RTSP test stream and you hit the rate limit. Try a local RTSP source;
+2. gstreamer does not work but the process won't terminate. Try `gst-launch-1.0 PIPELINE_DESCRIPTION` for more detailed error information.
+
+## Choppy video/audio
+
+Most times if you replace `rtspsrc` with a `filesrc` things will start to work. Therefore the choppiness is not due to the pipeline, but the source. Try adding `rtpjitterbuffer` to the pipe, and try different configurations of the buffer.
---
+++ streamer.py
@@ -0,0 +1,142 @@
+import random
+import ssl
+import websockets
+import asyncio
+import os
+import sys
+import json
+import argparse
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+gi.require_version('GstWebRTC', '1.0')
+from gi.repository import GstWebRTC
+gi.require_version('GstSdp', '1.0')
+from gi.repository import GstSdp
+
+PIPELINE_DESC = '''
+webrtcbin name=sendrecv stun-server=stun://stun.l.google.com:19302
+ rtspsrc location=rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov name=demuxer
+ demuxer. ! rtpjitterbuffer mode=0 ! queue ! parsebin ! rtph264pay config-interval=-1 timestamp-offset=0 !
+  queue ! application/x-rtp,media=video,encoding-name=H264,payload=98 ! queue ! sendrecv.
+ demuxer. ! rtpjitterbuffer mode=0 ! queue ! decodebin ! audioconvert ! audioresample ! opusenc ! rtpopuspay timestamp-offset=0 !
+  queue ! application/x-rtp,media=audio,encoding-name=OPUS,payload=96 ! queue ! sendrecv.
+'''
+
+class WebRTCClient:
+    def __init__(self, id_, peer_id, server):
+        self.id_ = id_
+        self.conn = None
+        self.pipe = None
+        self.webrtc = None
+        self.peer_id = peer_id
+        self.server = server or 'wss://webrtc.nirbheek.in:8443'
+
+    async def connect(self):
+        sslctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        self.conn = await websockets.connect(self.server, ssl=sslctx)
+        await self.conn.send('HELLO %d' % our_id)
+
+    async def setup_call(self):
+        await self.conn.send('SESSION {}'.format(self.peer_id))
+
+    def send_sdp_offer(self, offer):
+        text = offer.sdp.as_text()
+        print ('Sending offer:\n%s' % text)
+        msg = json.dumps({'sdp': {'type': 'offer', 'sdp': text}})
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(self.conn.send(msg))
+
+    def on_offer_created(self, promise, _, __):
+        promise.wait()
+        reply = promise.get_reply()
+        offer = reply.get_value('offer')
+        promise = Gst.Promise.new()
+        self.webrtc.emit('set-local-description', offer, promise)
+        promise.interrupt()
+        self.send_sdp_offer(offer)
+
+    def on_negotiation_needed(self, element):
+        promise = Gst.Promise.new_with_change_func(self.on_offer_created, element, None)
+        element.emit('create-offer', None, promise)
+
+    def send_ice_candidate_message(self, _, mlineindex, candidate):
+        icemsg = json.dumps({'ice': {'candidate': candidate, 'sdpMLineIndex': mlineindex}})
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(self.conn.send(icemsg))
+
+    def on_incoming_stream(self, _, pad):
+        if pad.direction != Gst.PadDirection.SRC:
+            return
+        fakesink = Gst.ElementFactory.make('fakesink')
+        self.pipe.add(fakesink)
+        fakesink.sync_state_with_parent()
+        self.webrtc.link(fakesink)
+
+    def start_pipeline(self):
+        self.pipe = Gst.parse_launch(PIPELINE_DESC)
+        self.webrtc = self.pipe.get_by_name('sendrecv')
+        self.webrtc.connect('on-negotiation-needed', self.on_negotiation_needed)
+        self.webrtc.connect('on-ice-candidate', self.send_ice_candidate_message)
+        self.webrtc.connect('pad-added', self.on_incoming_stream)
+        self.pipe.set_state(Gst.State.PLAYING)
+
+    async def handle_sdp(self, message):
+        assert (self.webrtc)
+        msg = json.loads(message)
+        if 'sdp' in msg:
+            sdp = msg['sdp']
+            assert(sdp['type'] == 'answer')
+            sdp = sdp['sdp']
+            print ('Received answer:\n%s' % sdp)
+            res, sdpmsg = GstSdp.SDPMessage.new()
+            GstSdp.sdp_message_parse_buffer(bytes(sdp.encode()), sdpmsg)
+            answer = GstWebRTC.WebRTCSessionDescription.new(GstWebRTC.WebRTCSDPType.ANSWER, sdpmsg)
+            promise = Gst.Promise.new()
+            self.webrtc.emit('set-remote-description', answer, promise)
+            promise.interrupt()
+        elif 'ice' in msg:
+            ice = msg['ice']
+            candidate = ice['candidate']
+            sdpmlineindex = ice['sdpMLineIndex']
+            self.webrtc.emit('add-ice-candidate', sdpmlineindex, candidate)
+
+    async def loop(self):
+        assert self.conn
+        async for message in self.conn:
+            if message == 'HELLO':
+                await self.setup_call()
+            elif message == 'SESSION_OK':
+                self.start_pipeline()
+            elif message.startswith('ERROR'):
+                print (message)
+                return 1
+            else:
+                await self.handle_sdp(message)
+        return 0
+
+
+def check_plugins():
+    needed = ["opus", "vpx", "nice", "webrtc", "dtls", "srtp", "rtp",
+              "rtpmanager", "videotestsrc", "audiotestsrc"]
+    missing = list(filter(lambda p: Gst.Registry.get().find_plugin(p) is None, needed))
+    if len(missing):
+        print('Missing gstreamer plugins:', missing)
+        return False
+    return True
+
+
+if __name__=='__main__':
+    Gst.init(None)
+    if not check_plugins():
+        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('peerid', help='String ID of the peer to connect to')
+    parser.add_argument('--server', help='Signalling server to connect to, eg "wss://127.0.0.1:8443"')
+    args = parser.parse_args()
+    our_id = random.randrange(10, 10000)
+    c = WebRTCClient(our_id, args.peerid, args.server)
+    asyncio.get_event_loop().run_until_complete(c.connect())
+    res = asyncio.get_event_loop().run_until_complete(c.loop())
+    sys.exit(res)
